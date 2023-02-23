5b316491a5e5b152db35c219c3b29ac713d04b62
Patryk <patryk.ciwinski@gmail.com>
2023-02-23 20:57:42 +0100

    lambda is easy

--- zadanie.py
+++ zadanie.py
@@ -1,4 +1,14 @@
+from functools import reduce
 
 list_nums=list(range(2,1000))
 primes = lambda x: all(map(lambda s: x % s, range(2, x)))
 print(list(filter(primes,list_nums)))
+
+
+nums=range(1,101)
+print(reduce(lambda x,y:x+y,nums))
+
+def fun(a,b):
+    return a+b
+#to jest to samo, muszę wcześniej stworzyć funkcję
+print(reduce(fun,nums))
