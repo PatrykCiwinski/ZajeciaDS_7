c14633fcff65eceec18570e78afefc18fed0dfc3
Patryk <patryk.ciwinski@gmail.com>
2023-02-23 18:00:39 +0100

    some function

---
+++ zajecia_7.py
@@ -0,0 +1,20 @@
+
+
+
+products = ['soap','sponge','brush','shower_gel','rubber_duck','shampoo']
+
+purchase=[]
+
+def buying(products, purchase):
+    while products:
+        stuff=products.pop()
+        print(f"buying {stuff}")
+        purchase.append(stuff)
+def show_purchase(purchase):
+    print(f"You bought: ")
+    for p in purchase:
+        print(p)
+
+buying(products,purchase)
+print("_________________")
+show_purchase(purchase)
